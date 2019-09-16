55b513f1a1dff72e1b3ff58362fec258ff2b2e70
Paul Brooks <paul@seru.systems>
2019-09-16 11:06:35 +0100

    Adding RefinedSugar (Syntactic)

---
+++ src/main/java/dev/kensa/RefinedSugar.java
@@ -0,0 +1,67 @@
+package dev.kensa;
+
+public final class RefinedSugar {
+
+    public static <T> T has(T t) {
+        return t;
+    }
+
+    public static <T> T have(T t) {
+        return t;
+    }
+
+    public static <T> T a(T t) {
+        return t;
+    }
+
+    public static <T> T an(T t) {
+        return t;
+    }
+
+    public static <T> T andA(T t) {
+        return t;
+    }
+
+    public static <T> T andAn(T t) {
+        return t;
+    }
+
+    public static <T> T andThe(T t) {
+        return t;
+    }
+
+    public static <T> T does(T t) {
+        return t;
+    }
+
+    public static <T> T in(T t) {
+        return t;
+    }
+
+    public static <T> T is(T t) {
+        return t;
+    }
+
+    public static <T> T of(T t) {
+        return t;
+    }
+
+    public static <T> T that(T t) {
+        return t;
+    }
+
+    public static <T> T the(T t) {
+        return t;
+    }
+
+    public static <T> T to(T t) {
+        return t;
+    }
+
+    public static <T> T with(T t) {
+        return t;
+    }
+
+    private RefinedSugar() {
+    }
+}
