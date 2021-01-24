02164ebfd21f6bb4931897662a85faebad12c663
Paul Brooks <paul@seru.systems>
2021-01-24 14:48:53 +0000

    Bump Kotlin, Gradle & JUnit versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,12 +2,12 @@ import org.gradle.api.JavaVersion
 
 object Versions {
 
-    val kotlin = "1.4.0"
+    const val kotlin = "1.4.21"
     val mainJavaVersion = JavaVersion.VERSION_1_8
     val testJavaVersion = JavaVersion.VERSION_11
 
     // Wrapper
-    const val gradleWrapper = "6.4.1"
+    const val gradleWrapper = "6.8.1"
 
     // Node
     const val node = "10.6.0"
@@ -16,12 +16,11 @@ object Versions {
     const val moowork = "2.2.0"
 
     // Dependencies
-    const val javaParser = "3.6.22"
     const val plantUml = "1.2020.23"
     const val pebble = "3.0.8"
     const val mockito = "2.24.0"
-    const val junitPlatform = "1.6.2"
-    const val junitJupiter = "5.6.2"
+    const val junitPlatform = "1.7.0"
+    const val junitJupiter = "5.7.0"
     const val assertJ = "3.11.1"
     const val hamcrest = "1.3"
     const val awaitility = "4.0.3"
