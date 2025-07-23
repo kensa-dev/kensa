868332e1498ed8464c8b55f154845a2be8d94122
Paul Brooks <paul@seru.systems>
2025-07-23 22:14:12 +0100

    add property to GivensContext (someone will ask for it anyway)

--- core/src/main/kotlin/dev/kensa/Action.kt
+++ core/src/main/kotlin/dev/kensa/Action.kt
@@ -4,7 +4,9 @@ import dev.kensa.fixture.Fixtures
 import dev.kensa.outputs.CapturedOutputs
 import dev.kensa.state.CapturedInteractions
 
-data class GivensContext(val fixtures: Fixtures, val outputs: CapturedOutputs)
+data class GivensContext(val fixtures: Fixtures, val outputs: CapturedOutputs) {
+    val fixturesAndOutputs = FixturesAndOutputs(fixtures, outputs)
+}
 data class ActionContext(val fixtures: Fixtures, val interactions: CapturedInteractions, val outputs: CapturedOutputs) {
     val fixturesAndOutputs = FixturesAndOutputs(fixtures, outputs)
 }
