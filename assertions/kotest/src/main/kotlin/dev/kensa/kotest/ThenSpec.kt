4e41af481858a0cbd49469c632b626d54d4ea092
Sonique <team.sonique.sns@bskyb.com>
2025-07-28 16:58:29 +0100

    Deprecating the other way around.

--- assertions/kotest/src/main/kotlin/dev/kensa/kotest/ThenSpec.kt
+++ assertions/kotest/src/main/kotlin/dev/kensa/kotest/ThenSpec.kt
@@ -4,10 +4,10 @@ import dev.kensa.CollectorContext
 import dev.kensa.StateCollector
 import io.kotest.matchers.Matcher
 
-@Deprecated("Use CollectingThenSpec instead", ReplaceWith("CollectingThenSpec<T>"))
-typealias ThenSpec<T> = CollectingThenSpec<T>
+@Deprecated("Use ThenSpec instead", ReplaceWith("dev.kensa.kotest.ThenSpec<T>"))
+typealias CollectingThenSpec<T> = ThenSpec<T>
 
-interface CollectingThenSpec<T> {
+interface ThenSpec<T> {
     val collector: StateCollector<T>
     val matcher: Matcher<T>
     val onMatch: CollectorContext.(T) -> Unit
