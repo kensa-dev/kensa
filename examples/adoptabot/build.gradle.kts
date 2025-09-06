3c4b2101684b9123cc2ec838fc1b8f18580d89ed
Paul Brooks <paul@seru.systems>
2025-09-06 12:50:26 +0100

    Bump Http4k for Kotest 6 support

--- examples/adoptabot/build.gradle.kts
+++ examples/adoptabot/build.gradle.kts
@@ -1,17 +1,10 @@
 dependencies {
     implementation(libs.http4kCore)
     implementation(libs.http4kJackson)
-    implementation(libs.http4kKotest) {
-        // Temporary until Http4k releases for Kotest 6.x
-        exclude(group = "io.kotest")
-    }
+    implementation(libs.http4kKotest)
 
     testImplementation(libs.http4kClientOkHttp)
     testImplementation(libs.jacksonKotlin)
-
-    // Temporary until Http4k releases for Kotest 6.x
-    testImplementation(platform(libs.kotestBom))
-    testImplementation(libs.kotestAssertionsCoreJvm)
 
     implementation(platform(libs.junitBom))
     implementation(libs.junitJupiterParams)
