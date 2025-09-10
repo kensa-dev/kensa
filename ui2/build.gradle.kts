7597c0bf156830bbf01dc56e4b34e730e9404f34
Paul Brooks <paul@seru.systems>
2025-09-10 10:03:50 +0100

    Release 0.5.24

---
+++ ui2/build.gradle.kts
@@ -0,0 +1,36 @@
+import com.github.gradle.node.task.NodeTask
+import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.tasks
+
+plugins {
+    alias(libs.plugins.nodeGradle)
+}
+
+node {
+    version = libs.versions.node.get()
+    download = true
+}
+
+tasks {
+    register<NodeTask>("viteBuild") {
+        script.set(project.file("node_modules/.bin/vite"))
+        args = listOf("build")
+
+        inputs.file("vite.config.ts")
+        inputs.file("package-lock.json")
+        inputs.dir("src")
+        outputs.dir("${layout.buildDirectory.get()}/js")
+        dependsOn("npmInstall")
+    }
+
+    register<NodeTask>("viteDev") {
+        script.set(project.file("node_modules/.bin/vite"))
+        args = listOf("dev")
+
+        inputs.file("vite.config.ts")
+        inputs.file("package-lock.json")
+        inputs.dir("src")
+
+        dependsOn("npmInstall")
+    }
+}
