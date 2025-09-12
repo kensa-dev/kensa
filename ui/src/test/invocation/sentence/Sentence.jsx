2efd2983cfbb4b8bf20f9465af632ecbab677a44
Sonique <team.sonique.sns@bskyb.com>
2025-09-12 10:05:49 +0100

    [Paul, Imke, Tim] :: Correcting the rendering of sentences with nested sentences.

--- ui/src/test/invocation/sentence/Sentence.jsx
+++ ui/src/test/invocation/sentence/Sentence.jsx
@@ -2,13 +2,32 @@ import React from "react";
 import "./Sentence.scss"
 import Token from "./Token";
 
-const Sentence = ({sentence}) =>
-    <div>
-        {
-            sentence.map((token, idx) =>
-                <Token key={idx} token={token}/>
-            ).reduce((prev, curr) => [prev, " ", curr])
-        }
-    </div>
+const Sentence = ({sentence}) => {
+
+    const containsNested = sentence.some(token => token.types.includes("tk-ex"))
+    const firstIsBlank = sentence[0].types.includes("tk-bl")
+
+    const withSpans = (tokens) => tokens.reduce((prev, curr) => [prev, <span className={"space"}/>, curr])
+    const withSpaces = (tokens) => tokens.reduce((prev, curr) => [prev, " ", curr])
+
+    const content = sentence.map((token, idx) =>
+        <Token key={idx} token={token}/>
+    )
+
+    return (
+        <div>
+            {firstIsBlank && containsNested ? (
+                <>
+                    {content[0]}
+                    {
+                        content.length > 1 && <div className="ns-container">{withSpans(content.slice(2))}</div>
+                    }
+                </>
+            ) : containsNested ? (
+                <div className="ns-container">{withSpans(content)}</div>
+            ) : (withSpaces(content))}
+        </div>
+    )
+}
 
 export default Sentence
