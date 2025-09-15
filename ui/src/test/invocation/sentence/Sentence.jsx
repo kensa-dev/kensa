8c7163876001f79bde0e479bde45310c91fa29ca
Sonique <team.sonique.sns@bskyb.com>
2025-09-15 12:25:10 +0100

    Fix cosmetics - no space to be inserted after the first tk-bl token.

--- ui/src/test/invocation/sentence/Sentence.jsx
+++ ui/src/test/invocation/sentence/Sentence.jsx
@@ -7,7 +7,12 @@ const Sentence = ({sentence}) => {
     const containsNested = sentence.some(token => token.types.includes("tk-ex"))
     const firstIsBlank = sentence[0].types.includes("tk-bl")
 
-    const withSpaces = (tokens) => tokens.flatMap((token, index) => index === 0 ? [token] : ['\u00A0', token]);
+    const withSpaces = (tokens) => tokens.flatMap((token, index) => {
+        if (index === 1 && tokens[0].props.token.types.includes("tk-bl")) {
+            return [token];
+        }
+        return index === 0 ? [token] : ['\u00A0', token];
+    });
 
     const content = sentence.map((token, idx) =>
         <Token key={idx} token={token}/>
