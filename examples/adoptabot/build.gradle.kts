64e2bf4f067c4eda35e26945bb34270f1d974b63
Paul Brooks <paul-brooks@users.noreply.github.com>
2026-03-24 12:59:11 +0000

    Correcting dependency scope

--- examples/adoptabot/build.gradle.kts
+++ examples/adoptabot/build.gradle.kts
@@ -1,15 +1,15 @@
 dependencies {
     implementation(libs.http4kCore)
     implementation(libs.http4kJackson)
-    implementation(libs.http4kKotest)
+    testImplementation(libs.http4kKotest)
 
     testImplementation(libs.http4kClientOkHttp)
     testImplementation(libs.jacksonKotlin)
 
-    implementation(platform(libs.junit6Bom))
-    implementation(libs.junit6JupiterParams)
-    implementation(libs.junit6JupiterApi)
-    implementation(libs.junit6JupiterEngine)
+    testImplementation(platform(libs.junit6Bom))
+    testImplementation(libs.junit6JupiterParams)
+    testImplementation(libs.junit6JupiterApi)
+    testImplementation(libs.junit6JupiterEngine)
 
     testImplementation(project(":assertions-hamcrest"))
     testImplementation(project(":assertions-kotest"))
