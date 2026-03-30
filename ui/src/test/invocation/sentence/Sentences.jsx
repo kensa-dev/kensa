8238a565636155a3801c5f708d5682d51eb0099f
Paul Brooks <paul-brooks@users.noreply.github.com>
2026-03-30 16:02:01 +0100

    Fix issue with legacy sentence rendering and new json format.

--- ui/src/test/invocation/sentence/Sentences.jsx
+++ ui/src/test/invocation/sentence/Sentences.jsx
@@ -5,7 +5,7 @@ const Sentences = ({invocation}) =>
     <div className="sentences">
         {
             invocation.sentences.map((sentence, idx) =>
-                <Sentence key={idx} sentence={sentence}/>)
+                <Sentence key={idx} sentence={sentence.tokens}/>)
         }
     </div>
 
